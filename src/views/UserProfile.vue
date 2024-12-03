42d652c31bac292968c4c1613b7fc7ae9e4cd4c3
LiaoKuanWei <jkk54623@gmail.com>
2024-12-03 11:13:28 +0800

    新增RWD

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <Header />
-        <div class="flex justify-center w-3/4 m-auto gap-9">
-            <div class="w-2/3 ">
+        <div class="sm:flex justify-center w-3/4 m-auto gap-9">
+            <div class="w-full sm:w-2/3 ">
                 <div class=" bg-slate-400 h-1/3">個人資料</div>
                 <UserTab />
             </div>
-            <div class="w-1/3 h-screen">
+            <div class=" my-10 w-full sm:w-1/3 h-screen">
                 <RatingCard />
             </div>
         </div>
