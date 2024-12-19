e2fa398959bd724fbac31de905a7d0f01ee6e527
James Hsiao <hsiaokuohsiang@gmail.com>
2024-12-19 20:47:52 +0800

    refactor(storepage): 將相似及推薦餐廳前後控制按鈕新增背景色及透明度，讓使用者體驗更好、新增RWD

--- src/views/StorePage.vue
+++ src/views/StorePage.vue
@@ -179,123 +179,166 @@ document.addEventListener('click', handleDocumentClick);
             <!-- 店家評論 -->
             <StoreComment/>
             <!-- 相似餐廳區 -->
-             <div class="md:w-[800px] mx-auto">
-
-                 <div class="mt-10 text-gray-700 md:w-[800px] mx-auto">
-                     <h3 class="mb-2 text-2xl font-bold">{{ storeName }} 的相似餐廳</h3>
-                     <div v-if="similarRestaurants && similarRestaurants.length" class="relative">
-                         <Carousel 
-                             :items-to-show="3"
-                             :wrap-around="true"
-                             :transition="500"
-                             snap-align="center"
-                             :itemsToScroll="3"
-                             :mouseDrag="true"
-                             class="flex justify-center"
-                         >
-                             <Slide 
-                                 v-for="restaurant in similarRestaurants.slice(0, 15)" 
-                                 :key="restaurant.place_id"
-                                 class="flex-shrink-0 px-1"
-                             >
-                                 <div class="bg-white rounded-lg shadow-md mb-4 max-w-[250px]">
-                                     <div class="overflow-hidden">
-                                         <img 
-                                             v-if="restaurant?.photoUrl" 
-                                             :src="restaurant.photoUrl" 
-                                             :alt="restaurant.name" 
-                                             class="object-cover w-[250px] h-[160px] rounded-t-lg" 
-                                         />
-                                     </div>
-                                     <div class="p-4">
-                                         <h4 class="text-lg font-bold truncate max-md:text-center">{{ restaurant?.name }}</h4>
-                                         <div class="flex flex-col items-center justify-between gap-2 mt-2 md:flex-row">
-                                             <p class="px-2 text-center text-white rounded-full bg-amber-500 w-fit">
-                                                 評分: {{ restaurant?.rating }}★
-                                             </p>
-                                             <p class="text-sm text-gray-400">
-                                                 {{ restaurant?.userRatingCount }}則評論
-                                             </p>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </Slide>
-                             
-                             <template #addons>
-                                 <Navigation />
-                             </template>
-                         </Carousel>
-                     </div>
-     
-                     <!-- 如果沒有數據顯示加載狀態 -->
-                     <div v-else class="py-4 text-center">
-                         正在加載餐廳資料...
-                     </div>
-                 </div>
-             </div>
-
-
-
-            <!-- 推薦餐廳部分 -->
-            <div class="mt-10 text-gray-700 md:w-[800px] mx-auto">
-                <h3 class="mb-2 text-2xl font-bold">推薦餐廳</h3>
-                <div v-if="recommendedRestaurants && recommendedRestaurants.length" class="relative">
-                    <Carousel 
-                        :items-to-show="3"
-                        :wrap-around="true"
-                        :transition="500"
-                        snap-align="center"
-                        :itemsToScroll="3"
-                        :mouseDrag="true"
-                        class="flex justify-center"
-                    >
-                        <Slide 
-                            v-for="restaurant in recommendedRestaurants" 
-                            :key="restaurant.place_id"
-                            class="flex-shrink-0 px-1"
+            <div class="md:w-[800px] mx-auto">
+                <div class="mt-10 text-gray-700 md:w-[800px] mx-auto">
+                    <h3 class="mb-2 text-2xl font-bold">{{ storeName }} 的相似餐廳</h3>
+                    <div v-if="similarRestaurants && similarRestaurants.length" class="relative">
+                        <Carousel 
+                            :breakpoints="{
+                                640: {
+                                    itemsToShow: 2,
+                                    snapAlign: 'start',
+                                },
+                                768: {
+                                    itemsToShow: 3,
+                                    snapAlign: 'center',
+                                }
+                            }"
+                            :wrap-around="true"
+                            :transition="500"
+                            :mouseDrag="true"
+                            :touchDrag="true"
+                            :gap="1"
+                            class="flex justify-center"
                         >
-                            <div class="bg-white rounded-lg shadow-md mb-4 max-w-[250px]">
-                                <div class="overflow-hidden">
-                                    <img 
-                                        v-if="restaurant?.photoUrl" 
-                                        :src="restaurant.photoUrl" 
-                                        :alt="restaurant.name" 
-                                        class="object-cover w-[250px] h-[160px] rounded-t-lg" 
-                                    />
-                                </div>
-                                <div class="p-4">
-                                    <h4 class="text-lg font-bold truncate max-md:text-center">{{ restaurant?.name }}</h4>
-                                    <div class="flex flex-col items-center justify-between gap-2 mt-2 md:flex-row">
-                                        <p class="px-2 text-center text-white rounded-full bg-amber-500 w-fit">
-                                            評分: {{ restaurant?.rating }}★
-                                        </p>
-                                        <p class="text-sm text-gray-400">
-                                            {{ restaurant?.userRatingCount }}則評論
-                                        </p>
+                            <Slide 
+                                v-for="restaurant in similarRestaurants.slice(0, 15)" 
+                                :key="restaurant.place_id"
+                                class="flex-shrink-0 px-1"
+                            >
+                                <div class="bg-white rounded-lg shadow-md mb-4 max-w-[250px]">
+                                    <div class="overflow-hidden">
+                                        <img 
+                                            v-if="restaurant?.photoUrl" 
+                                            :src="restaurant.photoUrl" 
+                                            :alt="restaurant.name" 
+                                            class="object-cover w-[250px] h-[160px] rounded-t-lg" 
+                                        />
+                                    </div>
+                                    <div class="p-4">
+                                        <h4 class="text-lg font-bold truncate max-md:text-center">{{ restaurant?.name }}</h4>
+                                        <div class="flex flex-col items-center justify-between gap-2 mt-2 md:flex-row">
+                                            <p class="px-2 text-center text-white rounded-full bg-amber-500 w-fit">
+                                                評分: {{ restaurant?.rating }}★
+                                            </p>
+                                            <p class="text-sm text-gray-400">
+                                                {{ restaurant?.userRatingCount }}則評論
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </Slide>
-                        
-                        <template #addons>
-                            <Navigation />
-                        </template>
-                    </Carousel>
-                </div>
-
-                <!-- 如果沒有數據顯示加載狀態 -->
-                <div v-else class="py-4 text-center">
-                    正在加載餐廳資料...
+                            </Slide>
+                            
+                            <template #addons>
+                                <Navigation>
+                                    <template #next>
+                                        <button class="bg-amber-500 opacity-80 rounded-full p-3 shadow-lg hover:bg-gray-300">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9 18L15 12L9 6" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </button>
+                                    </template>
+                                    <template #prev>
+                                        <button class="bg-amber-500 opacity-80 rounded-full p-3 shadow-lg hover:bg-gray-300">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M15 18L9 12L15 6" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </button>
+                                    </template>
+                                </Navigation>
+                            </template>
+                        </Carousel>
+                    </div>
+                    
+                    <!-- 如果沒有數據顯示加載狀態 -->
+                    <div v-else class="py-4 text-center">
+                        正在加載餐廳資料...
+                    </div>
                 </div>
             </div>
 
+            <!-- 推薦餐廳部分 -->
+            <div class="md:w-[800px] mx-auto">
+                <div class="mt-10 text-gray-700 md:w-[800px] mx-auto">
+                    <h3 class="mb-2 text-2xl font-bold">推薦餐廳</h3>
+                    <div v-if="recommendedRestaurants && recommendedRestaurants.length" class="relative">
+                        <Carousel 
+                            :breakpoints="{
+                                640: {
+                                    itemsToShow: 2,
+                                    snapAlign: 'start',
+                                },
+                                768: {
+                                    itemsToShow: 3,
+                                    snapAlign: 'center',
+                                }
+                            }"
+                            :wrap-around="true"
+                            :transition="500"
+                            :mouseDrag="true"
+                            :touchDrag="true"
+                            :gap="1"
+                            class="flex justify-center"
+                        >
+                            <Slide 
+                                v-for="restaurant in recommendedRestaurants" 
+                                :key="restaurant.place_id"
+                                class="flex-shrink-0 px-1"
+                            >
+                                <div class="bg-white rounded-lg shadow-md mb-4 max-w-[250px]">
+                                    <div class="overflow-hidden">
+                                        <img 
+                                            v-if="restaurant?.photoUrl" 
+                                            :src="restaurant.photoUrl" 
+                                            :alt="restaurant.name" 
+                                            class="object-cover w-[250px] h-[160px] rounded-t-lg" 
+                                        />
+                                    </div>
+                                    <div class="p-4">
+                                        <h4 class="text-lg font-bold truncate max-md:text-center">{{ restaurant?.name }}</h4>
+                                        <div class="flex flex-col items-center justify-between gap-2 mt-2 md:flex-row">
+                                            <p class="px-2 text-center text-white rounded-full bg-amber-500 w-fit">
+                                                評分: {{ restaurant?.rating }}★
+                                            </p>
+                                            <p class="text-sm text-gray-400">
+                                                {{ restaurant?.userRatingCount }}則評論
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Slide>
+                            
+                            <template #addons>
+                                <Navigation>
+                                    <template #next>
+                                        <button class="bg-amber-500 opacity-80 rounded-full p-3 shadow-lg hover:bg-gray-300">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9 18L15 12L9 6" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </button>
+                                    </template>
+                                    <template #prev>
+                                        <button class="bg-amber-500 opacity-80 rounded-full p-3 shadow-lg hover:bg-gray-300">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M15 18L9 12L15 6" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </button>
+                                    </template>
+                                </Navigation>
+                            </template>
+                        </Carousel>
+                    </div>
+    
+                    <!-- 如果沒有數據顯示加載狀態 -->
+                    <div v-else class="py-4 text-center">
+                        正在加載餐廳資料...
+                    </div>
+                </div>
+            </div>     
+
             <!-- 搜尋相關主題 -->
             <SearchTag/>
-
-
-
-
-        
+            
             <!-- 地圖區域 -->
             <div class="mt-10 text-gray-700">
                 <h3 class="mb-2 text-2xl font-bold">和牛涮 日式鍋物放題 台南中華西店 的食記</h3>
@@ -310,43 +353,7 @@ document.addEventListener('click', handleDocumentClick);
 
 
 <style scoped>
-.carousel__item {
-  width: 100%;
-}
 
-.carousel__slide {
-  width: 100%;
-}
-
-/* 自訂導航按鈕樣式 */
-.carousel__prev,
-.carousel__next {
-  background-color: rgb(245 158 11);
-  color: white;
-  border-radius: 9999px;
-  width: 2.5rem;
-  height: 2.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  z-index: 1;
-}
-
-.carousel__prev {
-  left: -3rem;
-}
-
-.carousel__next {
-  right: -2.5rem;
-}
-
-.carousel__prev:hover,
-.carousel__next:hover {
-  background-color: rgb(217 119 6);
-}
 
 
 </style>
